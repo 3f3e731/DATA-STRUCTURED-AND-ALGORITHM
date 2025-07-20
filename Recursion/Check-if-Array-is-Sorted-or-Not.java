ff26ad8f72b72e66315ba7ac53101aa715afdf53
Mridul Singh <mriduldhakad@gmail.com>
2025-07-20 19:37:41 +0530

    Add files via upload

---
+++ Recursion/Check-if-Array-is-Sorted-or-Not.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+    public static boolean isSorted(int []arr, int n){
+        if(n==arr.length-1){
+            return true;
+        }
+        if(arr[n]>arr[n+1]){
+            return false;
+        }
+        return isSorted(arr,n+1);
+    }
+
+    public static void main(String[] args) {
+        int[]arr={1,2,3,4,5,6};
+        int n=0;
+        System.out.println(isSorted(arr,n));
+    }
+}
