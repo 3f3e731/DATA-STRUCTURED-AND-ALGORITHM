73ba0ba2475e3d2e204a2be809388dcdcf7a1f85
Mridul Singh <mriduldhakad@gmail.com>
2025-12-21 13:00:37 +0530

    Update Target_Sum(Tabulation).java

--- Dynamic-Programming/Target_Sum(Tabulation).java
+++ Dynamic-Programming/Target_Sum(Tabulation).java
@@ -1,33 +1,54 @@
 import java.util.*;
 
 public class Main {
-    //Tabulation
-    public static boolean targetSum(int []arr,int sum){
-        int n=arr.length;
-        boolean dp[][]=new boolean[n+1][sum+1];
-
-        for(int i=0;i<n+1;i++){
-            dp[i][0]=true;
+    public static int targetSum(int []arr, int target, int sum ,int i,int [][]dp){
+        if(sum==target){
+            return 1;
         }
-        for(int i=1;i<n+1;i++){
-            for(int j=1;j<sum+1;j++){
-                int v=arr[i-1];
-                //include
-                if(v<=j && dp[i-1][j-v]==true){
-                    dp[i][j]=true;
-                }
-                //exclude
-                else if(dp[i-1][j]==true){
-                    dp[i][j]=true;
+        if(i==arr.length || sum>target){
+            return 0;
+        }
+        if(dp[i][sum]!=-1){
+            return dp[i][sum];
+        }
+        int ans1=targetSum(arr,target,sum+arr[i],i+1,dp);
+        int ans2=targetSum(arr, target, sum, i+1,dp);
+        dp[i][sum]= ans1+ans2;
+        return dp[i][sum];
+
+    }
+    public static int targetSumTab(int[]arr, int target){
+        int[][]dpTab=new int[arr.length+1][target+1];
+
+        for(int i=0;i<dpTab.length;i++){
+            dpTab[i][0]=1;
+        }
+        for(int i=1;i<dpTab[0].length;i++){
+            dpTab[0][i]=0;
+        }
+
+        for(int i=1;i<arr.length+1;i++){
+            for(int j=1;j<target+1;j++){
+                dpTab[i][j]=dpTab[i-1][j];
+
+                if(arr[i-1]<=j){
+                    dpTab[i][j]=dpTab[i][j]+dpTab[i-1][j-arr[i-1]];
                 }
             }
         }
-        return dp[n][sum];
+        return dpTab[arr.length][target];
     }
-
     public static void main(String[] args) {
         int[] arr={4,2,7,1,3};
-        int sum=10;
-        System.out.println(targetSum(arr,sum));
+        int target=10;
+        int [][]dp=new int[arr.length][target+1];
+        for(int i=0;i<dp.length;i++){
+            for(int j=0;j<dp[0].length;j++){
+                dp[i][j]=-1;
+            }
+        }
+        System.out.println(targetSum(arr,target,0,0,dp));
+
+        System.out.println(targetSumTab(arr,target));
     }
 }
