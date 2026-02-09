63108198b01cc64e0ffc29a3e86cc7bcabccade4
Mridul Singh <mriduldhakad@gmail.com>
2026-02-09 20:21:57 +0530

    Add files via upload

---
+++ BINARY-SERACH/Ceil_in_Sorted_Array.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Main{
+    public static int findCeil(int[] arr, int x) {
+        int n=arr.length;
+
+        int start=0,end=n-1;
+        int ans=-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+
+            if(arr[mid]>=x){
+                ans=mid;
+                end=mid-1;
+            }
+            else{
+                start=mid+1;
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        int[]arr={1, 2, 8, 10, 10, 12, 19};
+        int target=5;
+        System.out.println(findCeil(arr,target));
+    }
+}
